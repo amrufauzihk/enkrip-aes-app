416cff049a1b44064f6ed13466725a65683d8eaf
amru09 <amrufauzihk@gmail.com>
2022-08-21 11:40:16 +0800

    update database

--- admin/config/connect-db.php
+++ admin/config/connect-db.php
@@ -1,6 +1,6 @@
 <?php
 
-  $mysqli = mysqli_connect("localhost", "root", "", "db_aes", 3306); 
+  $mysqli = mysqli_connect("db4free.net", "adryan", "Muhammadadryan@9523", "db_aes", 3306); 
   
   if(!$mysqli){
   	die('error: '. mysqli_connect_error());
