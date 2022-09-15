a66005c4def35a3fd6c777552218adadaf63f4d1
amru09 <amrufauzihk@gmail.com>
2022-09-15 10:32:29 +0800

    update

--- admin/tambah_enkrip.php
+++ admin/tambah_enkrip.php
@@ -38,24 +38,9 @@
 
             if(!file.type.match('application/pdf*')) {
                 alert("only PDF File");
-                $("#form-id").get(0).reset(); //the tricky part is to "empty" the input file here I reset the form.
+                $("#form-id").get(0).reset();
                 return;
             }
-
-            // var fileReader = new FileReader();
-            // fileReader.onload = function(e) {
-            //     var int32View = new Uint8Array(e.target.result);
-            //     //verify the magic number
-            //     // for JPG is 0xFF 0xD8 0xFF 0xE0 (see https://en.wikipedia.org/wiki/List_of_file_signatures)
-            //     if(int32View.length>4 && int32View[0]==0xFF && int32View[1]==0xD8 && int32View[2]==0xFF && int32View[3]==0xE0) {
-            //         alert("ok!");
-            //     } else {
-            //         alert("only valid JPG images");
-            //         $("#form-id").get(0).reset(); //the tricky part is to "empty" the input file here I reset the form.
-            //         return;
-            //     }
-            // };
-            // fileReader.readAsArrayBuffer(file);
         });
     });
 </script>
